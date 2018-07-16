75ff9403ab4f18cbc2a65a2bacaba5b6d9451e4d
AgneloDias <agnelo.dias17@gmail.com>
2018-07-16 15:29:55 +0530

    Factorial Program

---
+++ Agnelo/src/git_1735/factorial.java
@@ -0,0 +1,18 @@
+package git_1735;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int i, fact=1;  
+		  int number=3;   
+		  
+		  for(i=1;i<=number;i++){    
+		      fact=fact*i;    
+		  }    
+		  System.out.println("Factorial of "+number+" is: "+fact);    
+		 }  
+		
+	}
+
+
